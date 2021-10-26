be7897b728ea61707025526e40326abffecb4c43
Nikolaos Dimopoulos <nikos@niden.net>
2021-10-25 20:10:55 -0400

    test(tests): added codeception structure

---
+++ tests/_bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+$root = dirname(realpath(__DIR__) . DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+//define('PROJECT_PATH', $root);
+
+require_once $root . 'tests/_config/functions.php';
+
+$folders = [
+    cacheDir(),
+    logsDir(),
+    outputDir('assets'),
+];
+
+foreach ($folders as $folder) {
+    if (true !== file_exists($folder)) {
+        mkdir($folder);
+    }
+}
+
+loadDefined();

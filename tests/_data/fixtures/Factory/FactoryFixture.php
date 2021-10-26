599f93bc5b2b4bb4dbeb89bd1b4c030120ee0c48
Nikolaos Dimopoulos <nikos@niden.net>
2021-10-25 20:18:17 -0400

    test(factory): added wrapper class for the factory trait and fixtures

---
+++ tests/_data/fixtures/Factory/FactoryFixture.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Fixtures\Factory;
+
+use Phalcon\Traits\Factory\FactoryTrait;
+
+class FactoryFixture
+{
+    use FactoryTrait;
+
+    /**
+     * @param array $services
+     */
+    public function __construct(array $services = [])
+    {
+        $this->init($services);
+    }
+    /**
+     * Create a new instance of the adapter
+     *
+     * @param string $name
+     * @param array  $options
+     *
+     * @throws FactoryExceptionFixture
+     */
+    public function newInstance(string $name, array $options = []): object
+    {
+        $definition = $this->getService($name);
+
+        return new $definition($options);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getExceptionClass(): string
+    {
+        return FactoryExceptionFixture::class;
+    }
+
+    /**
+     * @return string[]
+     */
+    protected function getServices(): array
+    {
+        return [
+            'one'   => FactoryOneFixture::class,
+            'two'   => FactoryTwoFixture::class,
+            'three' => FactoryThreeFixture::class,
+        ];
+    }
+}

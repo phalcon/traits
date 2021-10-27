96a987e664874ac37139159e818b41523a46342d
Nikolaos Dimopoulos <nikos@niden.net>
2021-10-27 09:22:12 -0400

    feat(helper/str/dirfromfile): added dirFromFile trait and tests

---
+++ src/Helper/Str/DirFromFileTrait.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of the Phalcon.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Traits\Helper\Str;
+
+use function implode;
+use function mb_str_split;
+use function mb_substr;
+use function pathinfo;
+
+use const PATHINFO_FILENAME;
+
+/**
+ * Accepts a file name (without extension) and returns a calculated
+ * directory structure with the filename in the end
+ */
+trait DirFromFileTrait
+{
+    /**
+     * @param string $file
+     *
+     * @return string
+     */
+    protected function toDirFromFile(string $file): string
+    {
+        $name  = pathinfo($file, PATHINFO_FILENAME);
+        $start = mb_substr($name, 0, -2);
+
+        if (!$start) {
+            $start = mb_substr($name, 0, 1);
+        }
+
+        return implode('/', mb_str_split($start, 2)) . '/';
+    }
+}

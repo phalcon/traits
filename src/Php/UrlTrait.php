4240f85bd60ec3a5dd50128e7ae88a6652e553f2
Nikolaos Dimopoulos <nikos@niden.net>
2021-10-30 07:15:38 -0400

    feat(php/urltrait): added url trait and relevant tests
    Refs #8

---
+++ src/Php/UrlTrait.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Traits\Php;
+
+use function base64_decode;
+use function base64_encode;
+use function parse_url;
+use function rawurldecode;
+use function rawurlencode;
+use function rtrim;
+use function strlen;
+use function substr;
+use function urldecode;
+use function urlencode;
+
+/**
+ * URL based wrapper methods.
+ */
+trait UrlTrait
+{
+    /**
+     * @param string $input
+     * @param bool   $strict
+     *
+     * @return string
+     */
+    protected function doBase64DecodeUrl(string $input, bool $strict = false): string
+    {
+        $input = strtr($input, "-_", "+/")
+            . substr("===", (strlen($input) + 3) % 4);
+
+        $result = $this->phpBase64Decode($input, $strict);
+        if (false === $result) {
+            return "";
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string $input
+     *
+     * @return string
+     */
+    protected function doBase64EncodeUrl(string $input): string
+    {
+        return rtrim(
+            strtr($this->phpBase64Encode($input), "+/", "-_"),
+            "="
+        );
+    }
+
+    /**
+     * @param string $input
+     * @param bool   $strict
+     *
+     * @return string|false
+     *
+     * @link https://www.php.net/manual/en/function.base64-decode.php
+     */
+    protected function phpBase64Decode(string $input, bool $strict = false)
+    {
+        return base64_decode($input, $strict);
+    }
+
+    /**
+     * @param string $input
+     *
+     * @return string
+     *
+     * @link https://www.php.net/manual/en/function.base64-encode.php
+     */
+    protected function phpBase64Encode(string $input): string
+    {
+        return base64_encode($input);
+    }
+
+    /**
+     * @param string $url
+     * @param int    $component
+     *
+     * @return array|false|int|string|null
+     *
+     * @link https://www.php.net/manual/en/function.parse-url.php
+     */
+    protected function phpParseUrl(string $url, int $component = -1)
+    {
+        return parse_url($url, $component);
+    }
+
+    /**
+     * @param string $string
+     *
+     * @return string
+     *
+     * @link https://www.php.net/manual/en/function.rawurldecode.php
+     */
+    protected function phpRawUrlDecode(string $string): string
+    {
+        return rawurldecode($string);
+    }
+
+    /**
+     * @param string $string
+     *
+     * @return string
+     *
+     * @link https://www.php.net/manual/en/function.rawurlencode.php
+     */
+    protected function phpRawUrlEncode(string $string): string
+    {
+        return rawurlencode($string);
+    }
+}
---
+++ tests/_data/fixtures/Php/UrlFixture.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Fixtures\Php;
+
+use Phalcon\Traits\Php\UrlTrait;
+
+class UrlFixture
+{
+    use UrlTrait;
+
+    /**
+     * @param string $input
+     * @param bool   $strict
+     *
+     * @return string
+     */
+    public function base64DecodeUrl(string $input, bool $strict = false): string
+    {
+        return $this->doBase64DecodeUrl($input, $strict);
+    }
+
+    /**
+     * @param string $input
+     *
+     * @return string
+     */
+    public function base64EncodeUrl(string $input): string
+    {
+        return $this->doBase64EncodeUrl($input);
+    }
+
+    /**
+     * @param string $input
+     * @param bool   $strict
+     *
+     * @return string|false
+     *
+     * @link https://www.php.net/manual/en/function.base64-decode.php
+     */
+    public function base64Decode(string $input, bool $strict = false)
+    {
+        return $this->phpBase64Decode($input, $strict);
+    }
+
+    /**
+     * @param string $input
+     *
+     * @return string
+     *
+     * @link https://www.php.net/manual/en/function.base64-encode.php
+     */
+    public function base64Encode(string $input): string
+    {
+        return $this->phpBase64Encode($input);
+    }
+
+    /**
+     * @param string $url
+     * @param int    $component
+     *
+     * @return array|false|int|string|null
+     *
+     * @link https://www.php.net/manual/en/function.parse-url.php
+     */
+    public function parseUrl(string $url, int $component = -1)
+    {
+        return $this->phpParseUrl($url, $component);
+    }
+
+    /**
+     * @param string $string
+     *
+     * @return string
+     *
+     * @link https://www.php.net/manual/en/function.rawurldecode.php
+     */
+    public function rawUrlDecode(string $string): string
+    {
+        return $this->phpRawUrlDecode($string);
+    }
+
+    /**
+     * @param string $string
+     *
+     * @return string
+     *
+     * @link https://www.php.net/manual/en/function.rawurlencode.php
+     */
+    public function rawUrlEncode(string $string): string
+    {
+        return $this->phpRawUrlEncode($string);
+    }
+}

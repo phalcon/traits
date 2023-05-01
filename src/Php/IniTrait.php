bef19aba35a0776cb134ea58e82d9283426b4bac
Nikolaos Dimopoulos <nikos@niden.net>
2023-05-01 09:55:00 -0400

    added static methods to ini trait

--- src/Php/IniTrait.php
+++ src/Php/IniTrait.php
@@ -34,6 +34,77 @@ trait IniTrait
         string $input,
         string $defaultValue = ""
     ): string {
+        return self::phpStaticIniGet($input, $defaultValue);
+    }
+
+    /**
+     * Query a php.ini value and return it back as boolean
+     *
+     * @param string $input
+     * @param bool   $defaultValue
+     *
+     * @return bool
+     *
+     * @link https://php.net/manual/en/function.ini-get.php
+     * @link https://php.net/manual/en/ini.list.php
+     */
+    protected function phpIniGetBool(
+        string $input,
+        bool $defaultValue = false
+    ): bool {
+        return self::phpStaticIniGetBool($input, $defaultValue);
+    }
+
+    /**
+     * Query a php.ini value and return it back as integer
+     *
+     * @param string $input
+     * @param int    $defaultValue
+     *
+     * @return int
+     *
+     * @link https://php.net/manual/en/function.ini-get.php
+     * @link https://php.net/manual/en/ini.list.php
+     */
+    protected function phpIniGetInt(string $input, int $defaultValue = 0): int
+    {
+        return self::phpStaticIniGetInt($input, $defaultValue);
+    }
+
+    /**
+     * Parse a configuration file
+     *
+     * @param string $filename
+     * @param bool   $processSections
+     * @param int    $scannerMode
+     *
+     * @return array|false
+     *
+     * @link https://php.net/manual/en/function.parse-ini-file.php
+     */
+    protected function phpParseIniFile(
+        string $filename,
+        bool $processSections = false,
+        int $scannerMode = 1
+    ) {
+        return self::phpStaticParseIniFile($filename, $processSections, $scannerMode);
+    }
+
+    /**
+     * Gets the value of a configuration option
+     *
+     * @param string $input
+     * @param string $defaultValue
+     *
+     * @return string
+     *
+     * @link https://php.net/manual/en/function.ini-get.php
+     * @link https://php.net/manual/en/ini.list.php
+     */
+    protected static function phpStaticIniGet(
+        string $input,
+        string $defaultValue = ""
+    ): string {
         $value = ini_get($input);
         if (false === $value) {
             return $defaultValue;
@@ -53,7 +124,7 @@ trait IniTrait
      * @link https://php.net/manual/en/function.ini-get.php
      * @link https://php.net/manual/en/ini.list.php
      */
-    protected function phpIniGetBool(
+    protected static function phpStaticIniGetBool(
         string $input,
         bool $defaultValue = false
     ): bool {
@@ -85,9 +156,11 @@ trait IniTrait
      * @link https://php.net/manual/en/function.ini-get.php
      * @link https://php.net/manual/en/ini.list.php
      */
-    protected function phpIniGetInt(string $input, int $defaultValue = 0): int
-    {
-        return (int)$this->iniGet($input, (string)$defaultValue);
+    protected static function phpStaticIniGetInt(
+        string $input,
+        int $defaultValue = 0
+    ): int {
+        return (int) self::phpStaticIniGet($input, (string)$defaultValue);
     }
 
     /**
@@ -101,7 +174,7 @@ trait IniTrait
      *
      * @link https://php.net/manual/en/function.parse-ini-file.php
      */
-    protected function phpParseIniFile(
+    protected static function phpStaticParseIniFile(
         string $filename,
         bool $processSections = false,
         int $scannerMode = 1
